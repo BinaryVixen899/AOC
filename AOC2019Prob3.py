dd9b1b8dea25fb4d759050edfaf70160e4054fe0
BinaryVixen899 <desertfox899@gmail.com>
2020-08-06 18:04:06 -0700

    test

--- AOC2019Prob3.py
+++ AOC2019Prob3.py
@@ -44,42 +44,81 @@
 # What is the Manhattan distance from the central port to the closest intersection?
 #  In a plane with p1 at (x1, y1) and p2 at (x2, y2), it is |x1 - x2| + |y1 - y2|.
 
-firstwirepath = ['R75','D30','R83','U83','L12','D49','R71','U7','L72']
-secondwirepath = ['U62','R66','U55','R34','D71','R55','D58','R83']
-firstwirepathrefined=[]
-secondwirepathrefined=[]
+#what if we just do the calculations 
+#like who cares about the historical points we can add those but
+#like right 0,0
+#then 75,0 
+#then 75 -30
+# 
+# Jsut need two functions, one that compares these and one that actually does the coordinates  
+
+#{0: [75, 0], 1: [75, -30], 2: [158, -30], 3: [158, 53], 4: [146, 53], 5: [146, 4], 6: [217, 4], 7: [217, 11], 8: [145, 11]}
+#{0: [0, 62], 1: [66, 62], 2: [66, 117], 3: [100, 117], 4: [100, 46], 5: [155, 46], 6: [155, -12], 7: [238, -12]}
+#WHOOPS THE LOGIC IS WRONg, this does NOT check where they cross because it does NOT look at where they are DURING it just calculates the coordinates
+#So what I would have to do would be to like, look at every single coordinate :(
+#honestly just use NUMPY
+
+
+#USE THIS ONE 
+# firstwirepath = 'R75,D30,R83,U83,L12,D49,R71,U7,L72'
+firstwirepath = 'U62,R66,U55,R34,D71,R55,D58,R83'
 pointax = 0 
 pointay = 0 
 pointbx = 0
 pointby = 0
-currentpointa = 0
-currentpointb = 0
+xcord = 0
+i = 0
+ycord = 0
+firstwirepath = firstwirepath.split(',')
+i = 0 
+
+dict = {}
+print(firstwirepath)
 
 for x in firstwirepath:
-    if x[1] == 'R':
-        firstwirepathrefined.append(x)
-    elif x[1] == 'L': 
-        x[1] = x[1] * -1
-        firstwirepathrefined.append(-x[1])
-    elif x[1] == 'U':
-        firstwirepathrefined.append(x)
-    elif x[1] == 'D':
-        firstwirepathrefined.append(-x[1])
 
-for x in firstwirepath:
-    pointax = currentpointa + x
-    pointay = firstwirepath[x+1]
-    currentpointa = [pointax,pointay]
+    if x[0] == 'R':
+        x = x.strip('R')
+        xcord += int(x) 
+        print(xcord)
+        dict[i] = [xcord,ycord]
+        print(dict)
+        i+=1
+        continue
+    elif x[0] == 'L': 
+        x = x.strip('L')
+        xcord += int(x) * -1
+        dict[i] = [xcord,ycord]
+        i+=1
+        continue
+    elif x[0] == 'U':
+        x = x.strip('U')
+        ycord += int(x)
+        dict[i] = [xcord,ycord]
+        i+=1
+        continue
+    elif x[0] == 'D':
+        x = x.strip('D')
+        ycord += int(x) * -1
+        dict[i] = [xcord,ycord]
+        i+=1
+        continue
+print(dict)
 
-for x in secondwirepath:
-    pointbx = currentpointb + x
-    pointby = secondwirepath[x+1]
-    currentpointb = [pointbx,pointby]
+# for x in firstwirepath:
+#     pointax = currentpointa + x
+#     pointay = firstwirepath[x+1]
+#     currentpointa = [pointax,pointay]
+
+# for x in secondwirepath:
+#     pointbx = currentpointb + x
+#     pointby = secondwirepath[x+1]
+#     currentpointb = [pointbx,pointby]
 
 
-if (currentpointa == currentpointb):
-    xdistance = pointax - pointay 
-    ydistane
+# if (currentpointa == currentpointb):
+#     xdistance = pointax - pointay 
+#     ydistane
 # firstwirepath = ['R75','D30','R83','U83','L12','D49','R71','U7','L72']
 # secondwirepath = ['U62','R66','U55','R34','D71','R55','D58','R83']
 
