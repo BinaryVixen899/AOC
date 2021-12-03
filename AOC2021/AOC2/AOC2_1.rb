e21cf94cca3d39dbb98b7f4e17bbc89013690b62
BinaryVixen899 <lealac@msn.com>
2021-12-02 22:22:20 -0800

    Organized and solved 2_1, plz NEVER use gvars irl

---
+++ AOC2021/AOC2/AOC2_1.rb
@@ -0,0 +1,84 @@
+# <!-- --- Day 2: Dive! ---
+# Now, you need to figure out how to pilot this thing.
+
+# It seems like the submarine can take a series of commands like forward 1, down 2, or up 3:
+
+# forward X increases the horizontal position by X units.
+# down X increases the depth by X units.
+# up X decreases the depth by X units.
+# Note that since you're on a submarine, down and up affect your depth, and so they have the opposite result of what you might expect.
+
+# The submarine seems to already have a planned course (your puzzle input). You should probably figure out where it's going. For example:
+
+# forward 5
+# down 5
+# forward 8
+# up 3
+# down 8
+# forward 2
+# Your horizontal position and depth both start at 0. The steps above would then modify them as follows:
+
+# forward 5 adds 5 to your horizontal position, a total of 5.
+# down 5 adds 5 to your depth, resulting in a value of 5.
+# forward 8 adds 8 to your horizontal position, a total of 13.
+# up 3 decreases your depth by 3, resulting in a value of 2.
+# down 8 adds 8 to your depth, resulting in a value of 10.
+# forward 2 adds 2 to your horizontal position, a total of 15.
+# After following these instructions, you would have a horizontal position of 15 and a depth of 10. (Multiplying these together produces 150.)
+
+# Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth? -->
+require 'pry' 
+$position = 0 
+$depth = 0 
+
+course = [
+"forward 5", 
+"down 5", 
+"forward 8",
+"up 3", 
+"down 8",
+"forward 2"]
+course = File.readlines("data.txt")
+
+# course = course.split(' ').strip
+
+def Calculate(direction, amount)
+  amount = amount.to_i
+  # if ['f', 'b', 'u', 'd'].include?(direction)
+  #   direction = direction
+  # end
+  puts amount
+
+
+  case direction
+
+  when 'f'
+    # binding.pry
+    $position += amount
+  
+  when 'u' 
+    $depth += (amount * -1)
+
+  when 'd'
+    $depth += amount
+ 
+  else
+    puts "Oh so there is a backwards"
+    
+  end
+end
+
+
+
+for i in course
+  direction = i[0]
+  i = i.split(" ")
+  amount = i[1]
+  Calculate(direction, amount)
+end
+
+puts ($position * $depth)
+
+
+
+# position * depth
