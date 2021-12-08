07e6a5936487ed59054e31ebc44a5e1bdb93be08
BinaryVixen899 <lealac@msn.com>
2021-12-07 22:27:48 -0800

    technically working

---
+++ AOC2021/AOC4/AOC4_1.rb
@@ -0,0 +1,22 @@
+numbers = [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1]
+
+bingoboard 
+#Okay so we have rows and columns 
+#I probably want to use pointers 
+#I need a thing for detecting if numbers are next to each other
+#Liz was right I'm going to have to use customer classes
+#This is going to be fun!
+
+
+bingocard = '22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19'
+ 
+# after every number drawn, check the adjacent numbers 
+# then check if any of those adjacent numbers are a match to the numbers we have already drawn 
+# if they are a match then add that to some sort of match tracker 
+# etc. 
+
+# 
