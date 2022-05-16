077d94d226e2a8d9141d686f479a3d07c22a52d0
BinaryVixen899 <lealac@msn.com>
2022-05-15 20:05:34 -0700

    fuel calculation step

--- AOC2021/AOC7/main.go
+++ AOC2021/AOC7/main.go
@@ -11,9 +11,10 @@ func main() {
 	// I don't know how large my input will be, hence a slice
 	inputslice := []int{16, 1, 2, 0, 4, 2, 7, 1, 2, 14}
 	mode = getmode(inputslice)
+	fuelcalculation(inputslice, mode)
 }
 
-func getmode(myslice []int) {
+func getmode(myslice []int) int {
 	// Obviously this makes a copy, but if I had bugs in this code I wouldn't want it to be able to change the values of the slice. Plus this allows us to change the values of the int
 
 	var greatestnumbervaluestring string
@@ -59,7 +60,8 @@ func getmode(myslice []int) {
 	}
 
 	// print(greatestnumbervaluestring)
-	print(greatestnumberkey)
+	gnk, _ := strconv.Atoi(greatestnumberkey)
+	return gnk
 
 	// Here is where we iterate over and increase the count
 
@@ -85,8 +87,19 @@ func unique(myslice []int) []int {
 
 func fuelcalculation(myslice []int, mode int) {
 	var fueltotal int
+	var addfuel int
 	for _, v := range myslice {
-		fueltotal += v - mode
+		addfuel = 0
+		addfuel = v - mode
+
+		if addfuel < 0 {
+			addfuel = addfuel * -1
+		}
+		print(addfuel)
+		fueltotal += addfuel
+
+		print(fueltotal)
+		print("\n")
 
 	}
 
